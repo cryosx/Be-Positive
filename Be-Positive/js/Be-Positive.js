04b0fb02910e6a687f58cf8e5b3c978f0ee516cc
cryosx <cyruswu.email@gmail.com>
2018-05-05 10:42:56 -1000

    finished

--- Be-Positive/js/Be-Positive.js
+++ Be-Positive/js/Be-Positive.js
@@ -1,23 +1,57 @@
 BloodType = {
+  AB_POS: 'AB_POS',
+  AB_NEG: 'AB_NEG',
+  A_POS: 'A_POS',
+  A_NEG: 'A_NEG',
+  B_POS: 'B_POS',
+  B_NEG: 'B_NEG',
+  O_POS: 'O_POS',
+  O_NEG: 'O_NEG'
+};
 
-  AB_POS : "AB_POS",
-  AB_NEG : "AB_NEG",
-  A_POS  : "A_POS",
-  A_NEG  : "A_NEG",
-  B_POS  : "B_POS",
-  B_NEG  : "B_NEG",
-  O_POS  : "O_POS",
-  O_NEG  : "O_NEG"
+// Transfusion = {
+//   AB_POS: [
+//     BloodType.AB_POS,
+//     BloodType.AB_NEG,
+//     BloodType.A_POS,
+//     BloodType.A_NEG,
+//     BloodType.B_POS,
+//     BloodType.B_NEG,
+//     BloodType.O_POS,
+//     BloodType.O_NEG
+//   ],
+//   AB_NEG: [BloodType.AB_NEG, BloodType.A_NEG, BloodType.B_NEG, BloodType.O_NEG],
+//   A_POS: [BloodType.A_POS, BloodType.A_NEG, BloodType.O_POS, BloodType.O_NEG],
+//   A_NEG: [BloodType.A_NEG, BloodType.O_NEG],
+//   B_POS: [BloodType.B_POS, BloodType.B_NEG, BloodType.O_POS, BloodType.O_NEG],
+//   B_NEG: [BloodType.B_NEG, BloodType.O_NEG],
+//   O_POS: [BloodType.O_POS, BloodType.O_NEG],
+//   O_NEG: [BloodType.O_NEG]
+// };
 
+Transfusion = {
+  AB_POS: [
+    BloodType.AB_POS,
+    BloodType.A_POS,
+    BloodType.B_POS,
+    BloodType.O_POS
+    // BloodType.O_NEG
+  ],
+  AB_NEG: [BloodType.AB_NEG, BloodType.A_NEG, BloodType.B_NEG, BloodType.O_NEG],
+  A_POS: [BloodType.A_POS, BloodType.A_NEG, BloodType.O_POS],
+  A_NEG: [BloodType.A_NEG, BloodType.O_NEG],
+  B_POS: [BloodType.B_POS, BloodType.B_NEG, BloodType.O_POS],
+  B_NEG: [BloodType.B_NEG, BloodType.O_NEG],
+  O_POS: [BloodType.O_POS, BloodType.O_NEG],
+  O_NEG: [BloodType.O_NEG]
 };
 
 BloodTransfusionRules = {
-
   /**
    * Set the simulation speed.
    * @type {Number} : Valid values between 1 and 200
    */
-  simulation_speed : 90,
+  simulation_speed: 200,
 
   /**
    * returns BloodType, or false to give no BloodType
@@ -45,17 +79,52 @@ BloodTransfusionRules = {
    *
    */
 
-  receive_patient : function (blood_inventory, patient) {
+  receive_patient: function(blood_inventory, patient) {
+    if (
+      patient.blood_type === BloodType.AB_POS &&
+      blood_inventory[BloodType.AB_POS] > 0
+    ) {
+      return BloodType.AB_POS;
+    }
+    if (
+      patient.blood_type === BloodType.A_POS &&
+      blood_inventory[BloodType.A_POS] > 0
+    ) {
+      return BloodType.A_POS;
+    }
+    if (
+      patient.blood_type === BloodType.B_POS &&
+      blood_inventory[BloodType.B_POS] > 0
+    ) {
+      return BloodType.B_POS;
+    }
 
-    // give a random blood type to anyone
-    // very bad idea!
-    return [
-      BloodType.AB_POS,
-      BloodType.AB_NEG,
-      BloodType.A_POS,
-      BloodType.A_NEG
-    ][Math.floor(Math.random()*4)];
+    return getMostAvailable(blood_inventory, patient);
 
+    function getMostAvailable(blood_inventory, patient) {
+      let bloodTypes = Transfusion[patient.blood_type];
+      let mostAvailableBloodType = bloodTypes[0];
+      for (let i = 0; i < bloodTypes.length; i++) {
+        if (
+          blood_inventory[bloodTypes[i]] >
+          blood_inventory[mostAvailableBloodType]
+        ) {
+          mostAvailableBloodType = bloodTypes[i];
+        }
+      }
+      return mostAvailableBloodType;
+    }
+    function getRandom() {
+      return [
+        BloodType.AB_POS,
+        BloodType.AB_NEG,
+        BloodType.A_POS,
+        BloodType.A_NEG,
+        BloodType.B_POS,
+        BloodType.B_NEG,
+        BloodType.O_POS,
+        BloodType.O_NEG
+      ][Math.floor(Math.random() * 8)];
+    }
   }
-
 };
